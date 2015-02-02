ba0e0cc6301b404b28d5562184e955f487f6641b
Termanty <termanty@cs.helsinki.fi>
2015-02-02 09:44:28 +0200

    checking

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,6 +4,8 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validates :name, presence: true,
+
   def print_report
     puts name
     puts "established at year #{year}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
-  validates :username, uniqueness: true, length: { minimum: 3 }
+  validates :username, uniqueness: true, length: { minimum: 3,  maximum: 15 }
 
-  has_many :ratings
+  has_many :ratings ,numericality: { greater_than_or_equal_to: 1024,
+                                    less_than_or_equal_to: ->(_){Time.now.year} }
+
+
 end
