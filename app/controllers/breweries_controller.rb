4af07268106f460469da1b72508b01fb1e00b7cd
Termanty <termanty@cs.helsinki.fi>
2015-02-18 18:41:24 +0200

    Activity feature added for breweries. Rspec now broken.

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -5,7 +5,10 @@ class BreweriesController < ApplicationController
   # GET /breweries
   # GET /breweries.json
   def index
-    @breweries = Brewery.all
+    def index
+      @active_breweries = Brewery.active
+      @retired_breweries = Brewery.retired
+    end
   end
 
   # GET /breweries/1
@@ -70,7 +73,7 @@ class BreweriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
-      params.require(:brewery).permit(:name, :year)
+      params.require(:brewery).permit(:name, :year, :activity)
     end
 
     def authenticate
